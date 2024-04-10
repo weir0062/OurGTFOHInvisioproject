be242444c26257d7635941584e71c949219206db
weir0062 <Tanichka1>
2024-04-10 17:57:52 -0400

    Main Menu optimized

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject mainMenu;
+    public GameObject ShoesMenu;
+
+    public void OpenShoes()
+    {
+        mainMenu.SetActive(false);
+        ShoesMenu.SetActive(true);
+    }
+    public void OpenMainMenu()
+    {
+        mainMenu.SetActive(true);
+        ShoesMenu.SetActive(false);
+
+    }
+    void Start()
+    {
+        OpenMainMenu();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
