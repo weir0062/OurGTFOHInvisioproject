5e522419ab4b9d1e093fa3d19a25bb6fcd91589a
weir0062 <Tanichka1>
2024-04-15 14:02:55 -0400

    .

--- Assets/Scripts/VibrationHandler.cs
+++ Assets/Scripts/VibrationHandler.cs
@@ -1,45 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using CandyCoded.HapticFeedback;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.UI;
+//using CandyCoded.HapticFeedback;
 
-public class VibrationHandler : MonoBehaviour
-{
-    [SerializeField]
-    private Button defaultVibrationButton;
+//public class VibrationHandler : MonoBehaviour
+//{
+//    [SerializeField]
+//    private Button defaultVibrationButton;
 
-    private void OnEnable()
-    {
-        defaultVibrationButton.onClick.AddListener(DefaultVibration);
-    }
+//    private void OnEnable()
+//    {
+//        defaultVibrationButton.onClick.AddListener(DefaultVibration);
+//    }
 
-    private void OnDisable()
-    {
-        defaultVibrationButton.onClick.RemoveListener(DefaultVibration);
-    }
+//    private void OnDisable()
+//    {
+//        defaultVibrationButton.onClick.RemoveListener(DefaultVibration);
+//    }
         
-    private void DefaultVibration()
-    {
-        Debug.Log("Default Vibration performed.");
-        Handheld.Vibrate();
-    }
+//    private void DefaultVibration()
+//    {
+//        Debug.Log("Default Vibration performed.");
+//        Handheld.Vibrate();
+//    }
 
-    private void LightVibration()
-    {
-        Debug.Log("Light Vibration performed.");
-        HapticFeedback.LightFeedback();
-    }
+//    private void LightVibration()
+//    {
+//        Debug.Log("Light Vibration performed.");
+//        HapticFeedback.LightFeedback();
+//    }
 
-    private void MediumVibration()
-    {
-        Debug.Log("Medium Vibration performed.");
-        HapticFeedback.MediumFeedback();
-    }
+//    private void MediumVibration()
+//    {
+//        Debug.Log("Medium Vibration performed.");
+//        HapticFeedback.MediumFeedback();
+//    }
 
-    private void HeavyVibration()
-    {
-        Debug.Log("Heavy Vibration performed.");
-        HapticFeedback.HeavyFeedback();
-    }
-}
+//    private void HeavyVibration()
+//    {
+//        Debug.Log("Heavy Vibration performed.");
+//        HapticFeedback.HeavyFeedback();
+//    }
+//}
